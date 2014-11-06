57b0e0b3a3ce0f70da88d1fbf117e65cac322c70
Austin Tooley <tooley7@gmail.com>
2014-11-06 11:06:40 -0700

    Added Entity and working on |> operator

---
+++ FunctionalDiscreteEventEngine/PipelineOpperator.swift
@@ -0,0 +1,20 @@
+//
+//  PipelineOpperator.swift
+//  FunctionalDiscreteEventEngine
+//
+//  Created by Austin Tooley on 11/6/14.
+//  Copyright (c) 2014 Austin Tooley. All rights reserved.
+//
+
+import Foundation
+
+//accept a value (left) and use it in the function (right)
+infix operator |> { associativity left }
+func |> <T,U>(left: T, right: T -> U) -> U {
+    return right(left)
+}
+
+
+func |> <T>(left: ((String?, Entity)->T, Entity, (String?, Any)->()), var right: ()->() ) -> (T){
+
+}
