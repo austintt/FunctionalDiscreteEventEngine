55afda0f79a3dc59ad19dd8bf3aefe2f8f03e37b
Austin Tooley <tooley7@gmail.com>
2014-12-04 10:17:46 -0700

    Added altMap to driver, engine not running alts...

--- FunctionalDiscreteEventEngine/Driver.swift
+++ FunctionalDiscreteEventEngine/Driver.swift
@@ -9,42 +9,76 @@
 import Foundation
 
 //create graphs
-func startEngine() {
-    var myGraph = Graph()
+struct Driver {
+    
+    func startEngine() {
+        var myGraph = Graph()
 
-    //fill entities array
-    myGraph.entities = [Entity(number: 0), Entity(number: 1), Entity(number: 2), Entity(number: 3), Entity(number: 4), Entity(number: 5)]
-    
-    //fill mainPath array
-    myGraph.mainPath = buildGraphlet(
-        {(anEntity: Entity) -> (Entity) in dispatch_async(dispatch_get_main_queue(), { () -> Void in
-            println("Main event 0 peformed on entity number \(anEntity.number)")
-        })
-        return anEntity},
-        {(anEntity: Entity) -> (Entity) in dispatch_async(dispatch_get_main_queue(), { () -> Void in
-            println("Main event 1 peformed on entity number \(anEntity.number)")
-        })
-            return anEntity},
-        {(anEntity: Entity) -> (Entity) in dispatch_async(dispatch_get_main_queue(), { () -> Void in
-            println("Main event 2 peformed on entity number \(anEntity.number)")
-        })
-            return anEntity},
-        {(anEntity: Entity) -> (Entity) in dispatch_async(dispatch_get_main_queue(), { () -> Void in
-            println("Main event 3 peformed on entity number \(anEntity.number)")
-        })
-            return anEntity}) 
+        //fill entities array
+        myGraph.entities = [Entity(number: 0), Entity(number: 1), Entity(number: 2), Entity(number: 3), Entity(number: 4), Entity(number: 5)]
 
-    //fill altMap
+        //fill mainPath array
+        myGraph.mainPath = buildGraphlet(
+            event0,
+            event1,
+            event2,
+            event3)
 
-    //array - why?
-    let graphs = [myGraph]
+        //fill altMap
+        myGraph.alternateMap = [buildGraphlet(event2)[0]: buildGraphlet(
+            altEvent0,
+            altEvent1,
+            altEvent2,
+            altEvent3)]
+
+        //array - why?
+        let graphs = [myGraph]
+
+        //create engine
+        var engine = SimulationEngine()
+        engine.graphs = graphs
+
+        //run engine
+        engine.go(1)
+    }
     
-    //create engine
-    var engine = SimulationEngine()
-    engine.graphs = graphs
+    //MAIN EVENTS
+    let event0 = {(anEntity: Entity) -> (Entity) in dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        println("Main event 0 peformed on entity number \(anEntity.number)")
+    })
+        return anEntity}
     
-    //run engine
-    engine.go(1)
+    let event1 = {(anEntity: Entity) -> (Entity) in dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        println("Main event 1 peformed on entity number \(anEntity.number)")
+    })
+        return anEntity}
+    
+    let event2 = {(anEntity: Entity) -> (Entity) in dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        println("Main event 2 peformed on entity number \(anEntity.number)")
+    })
+        return anEntity}
+    
+    let event3 = {(anEntity: Entity) -> (Entity) in dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        println("Main event 3 peformed on entity number \(anEntity.number)")
+    })
+        return anEntity}
     
     
+    //ALT EVENTS
+    let altEvent0 = {(anEntity: Entity) -> (Entity) in dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        println("\tAlternate event 0 peformed on entity number \(anEntity.number)")
+    })
+        return anEntity}
+    let altEvent1 = {(anEntity: Entity) -> (Entity) in dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        println("\tAlternate event 1 peformed on entity number \(anEntity.number)")
+    })
+        return anEntity}
+    let altEvent2 = {(anEntity: Entity) -> (Entity) in dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        println("\tAlternate event 2 peformed on entity number \(anEntity.number)")
+    })
+        return anEntity}
+    let altEvent3 = {(anEntity: Entity) -> (Entity) in dispatch_async(dispatch_get_main_queue(), { () -> Void in
+        println("\tAlternate event 3 peformed on entity number \(anEntity.number)")
+    })
+        return anEntity}
 }
