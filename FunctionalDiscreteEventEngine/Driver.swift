e618e841e508bac224abbb60dc06314887b89bd3
Austin Tooley <tooley7@gmail.com>
2014-12-16 11:01:46 -0700

    Working!

--- FunctionalDiscreteEventEngine/Driver.swift
+++ FunctionalDiscreteEventEngine/Driver.swift
@@ -60,15 +60,17 @@ struct Driver {
     let event1 = {(var anEntity: Entity, uuid: NSUUID) -> (Entity, Bool) in
         dispatch_async(dispatch_get_main_queue(), { () -> Void in
             println("Main event 1 peformed on entity number \(anEntity.number)")})
-            
+        
+        anEntity.numOfActionsPerformed++
+        dispatch_async(dispatch_get_main_queue(), {println("\t\tEntity \(anEntity.number) has been through action1 \(anEntity.numOfActionsPerformed) times")})
+        
             //set fail flag for testing
             let randNum = Int(arc4random_uniform(10))
             if (randNum % 5) == 0 {
-                println("We failed!")
+                println("We failed on entity \(anEntity.number)")
                 return (anEntity, true)
             }
-        anEntity.numOfActionsPerformed++
-        dispatch_async(dispatch_get_main_queue(), {println("\t\tEntity \(anEntity.number) has been through action1 \(anEntity.numOfActionsPerformed) times")})
+        
         return (anEntity, false)}
     
     let event2 = {(anEntity: Entity, uuid: NSUUID) -> (Entity, Bool) in
